8d718735cc1442711bb44df886fa02473c858f3b
Kris Northfield <kris@quarterdeck.co.uk>
2023-08-10 10:07:20 +0100

    boot

---
+++ NpsCollectionMacroServiceProvider.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Quarterdeck;
+
+use Illuminate\Support\ServiceProvider;
+
+class QuarterdeckUIServiceProvider extends ServiceProvider
+{
+	public function boot()
+	{
+		//
+	}
+
+	public function register()
+	{
+		Collection::macro('nps', function () {
+			return $this->whereInStrict(null, range(0, 10))
+				// ->filter(fn($score) => in_array($score, range(0,10), true))
+				->map(function ($score) {
+					return match ($score) {
+						0,1,2,3,4,5,6 => 'detractor',
+						7,8 => 'passive',
+						9,10 => 'promoter',
+					};
+				})
+				->countBy()
+				->pipe(function ($scores) {
+					if ($scores->isEmpty()) {
+						return 0;
+					}
+
+					$sum = $scores->sum();
+
+					$percent_promoters = $scores['promoter'] / $sum;
+					$percent_detractors = $scores['detractor'] / $sum;
+					$nps = ($percent_promoters - $percent_detractors) * 100;
+
+					return intval(round($nps));
+				});
+		});
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# laravel-collection-nps
-Calculate Net Promoter Score (NPS) from a Collection
+# Laravel Collection Net Promoter Score Macro
+
+Calculate Net Promoter Score (NPS) from a Collection.
+
+Given a collection of numbers just add `->nps()` and it will spit out an NPS score.
+
+`collect([1,2,3,4,5,6,7,8,9,0,10])->nps();`
+
+Any values not in the range of integers from 0-10 will be ignored.
+
+`collect([1,2,3,4,5,6,7,8,9,0,10,-1,99,null,false,'boo'])->nps();`
+
+## License
+
+License is public domain. Do what you wish. But if you fancy linking to us at `quarterdeck.co.uk` to help with our Google Juice as a thank you then it would be very much appreciated.
+
+Be lucky.
